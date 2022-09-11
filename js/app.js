13c3a58c161554842b44f47d44057b6152901bbc
oniel santigo <onielst84@gmail.com>
2022-09-11 19:35:12 -0300

    adicionada funcionalidade de deletar e editar

--- js/app.js
+++ js/app.js
@@ -6,10 +6,13 @@ const fechaInput = document.querySelector('#fecha');
 const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
 
+let editando;
+
 //UI
 const formulario = document.querySelector('#nueva-cita');
 const contenedorCitas = document.querySelector('#citas');
 
+//clases
 class Citas {
   constructor(){
     this.citas = [];
@@ -18,6 +21,14 @@ class Citas {
   adicionarCita(cita){
     this.citas = [...this.citas, cita];
     // console.log(this.citas);
+  }
+
+  eliminarCita(id){
+    this.citas = this.citas.filter(cita => cita.id !== id);
+  }
+
+  editarCita(citaAtualizada){
+    this.citas = this.citas.map(cita => cita.id === citaAtualizada.id ? citaAtualizada : cita)
   }
 }
 
@@ -32,7 +43,7 @@ class UI {
     if(tipo === "error"){
       divMensagem.classList.add('alert-danger');
     } else {
-      divMensagem.classList.add('alert-seccess');
+      divMensagem.classList.add('alert-success');
     }
   
     //mensagemd de erro
@@ -88,6 +99,23 @@ class UI {
         <span class='font-weight-bolder'>Sintomas: </span> ${sintomas}
       `;
 
+      //boton para eliminar esta cita
+      const btnEliminar = document.createElement('button');
+      btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+      btnEliminar.innerHTML = `Deletar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`;
+      btnEliminar.onclick = () => eliminarCita(id);
+
+
+      //boton para editar a esta cita
+      const btnEditar = document.createElement('button');
+      btnEditar.classList.add('btn', 'btn-info');
+      btnEditar.innerHTML = `Editar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" /></svg>`;
+      btnEditar.onclick = () => carregarEdicao(cita);
+      
+
+      
       //adicionar os parrafos ao divCita
       divCita.appendChild(mascotaParrafo);
       divCita.appendChild(propietarioParrafo);
@@ -95,6 +123,8 @@ class UI {
       divCita.appendChild(fechaParrafo);
       divCita.appendChild(horaParrafo);
       divCita.appendChild(sintomasParrafo);
+      divCita.appendChild(btnEliminar);
+      divCita.appendChild(btnEditar);
 
       //adicionar as citas ao HTML
       contenedorCitas.appendChild(divCita);
@@ -153,11 +183,31 @@ function nuevaCita(e){
     return;
   }
 
-  //gerar um id único
-  citaObj.id = Date.now();
+  //validar
+  if(editando){
 
-  //criando uma nova cita
-  administrarCitas.adicionarCita({...citaObj});
+    ui.imprimirAlerta('Cita editada corretamente');
+
+    //envir o objeto da cita para a edição
+    administrarCitas.editarCita({...citaObj})
+
+    //mudar o texto do boton
+    formulario.querySelector('button[type="submit"]').textContent = "CRIAR CITA";
+
+    //sair do modo edição
+    editando = false;
+
+  } else {
+    //gerar um id único
+    citaObj.id = Date.now();
+
+    //criando uma nova cita
+    administrarCitas.adicionarCita({...citaObj});
+
+    ui.imprimirAlerta('Cita adicionada corretamente');
+  }
+
+  
 
   //reiniciar o objeto
   reiniciarObjeto();
@@ -178,4 +228,43 @@ function reiniciarObjeto(){
   citaObj.hora = '';
   citaObj.sintomas = '';
 
+}
+
+//eliminar cita
+function eliminarCita(id){
+  //elimianr a cita
+  administrarCitas.eliminarCita(id)
+
+  //mostrar mensagem
+  ui.imprimirAlerta("Cita deletada corretamente");
+
+  //atualizar as citas
+  ui.imprimirCitas(administrarCitas);
+}
+
+//carregar os dados e o modo edicao
+function carregarEdicao(cita){
+  
+  const { mascota, propietario, telefono, fecha, hora, sintomas, id } = cita;
+
+  //preencher os inputs
+  mascotaInput.value = mascota; 
+  propietarioInput.value = propietario;
+  telefonoInput.value = telefono;
+  fechaInput.value = fecha;
+  horaInput.value = hora;
+  sintomasInput.value = sintomas;
+
+  citaObj.mascota = mascota;
+  citaObj.propietario = propietario;
+  citaObj.telefono = telefono;
+  citaObj.fecha = fecha;
+  citaObj.hora = hora;
+  citaObj.sintomas = sintomas;
+  citaObj.id = id;
+
+  //mudar o texto do boton
+  formulario.querySelector('button[type="submit"]').textContent = "Salvar";
+
+  editando = true;
 }
